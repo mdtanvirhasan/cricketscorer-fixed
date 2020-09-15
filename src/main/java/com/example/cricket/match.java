21baabaafc6e900b0fba6116edfa1ab75ce8b1f8
mdtanvirhasan <tanvirhasan111753@gmail.com>
2020-09-16 01:32:44 +0600

    taking input

--- src/main/java/com/example/cricket/match.java
+++ src/main/java/com/example/cricket/match.java
@@ -3,12 +3,37 @@ package com.example.cricket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class match extends AppCompatActivity {
+
+    TextView runs;
+    TextView wickets;
+    TextView overs;
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button6;
+    Button button4;
+    Button buttonwide;
+    Button buttonno;
+
+    public match() {
+        button2 = new Button();
+        button1 = new Button();
+        button3 = new Button();
+        button6 = new Button();
+        button4 = new Button();
+        buttonwide = new Button();
+        buttonno = new Button();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
     }
+
 }
